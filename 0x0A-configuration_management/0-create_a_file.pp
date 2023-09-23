78e4f1287107747e4e2ea84ef1cc9884eccef365
Starjakes <amehjacobe@gmail.com>
2023-09-22 19:26:50 -0700

    0. Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-file { 'school':
-  ensure  => 'present',
-  content => 'I love Puppet',
-  group   => 'www-data',
-  mode    => '0744',
-  owner   => 'www-data',
-  path    => '/tmp/school',
-}
+# Define a defined resource type to manage the 'school' file
+file { '/tmp/school':
+    ensure  => 'present',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet', }
