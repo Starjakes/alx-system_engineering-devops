fd9da9210faab1399461dd5b8662e79a92e76e17
Starjakes <amehjacobe@gmail.com>
2023-10-01 17:51:07 -0700

    5. Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,33 @@
-# Install Nginx web server (w/ Puppet)
-exec { 'server configuration':
+# Manifest to install and configure an Nginx server using Puppet instead of Bash
+package {'nginx':
+  ensure => 'present',
+}
+exec {'update apt-get and install':
+    command  => 'sudo apt-get update && sudo apt-get install nginx',
+    path     => '/usr/bin:/usr/sbin:/bin',
+    provider => shell
+}
+
+exec {'add landing page':
+    command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+    path     => '/usr/bin:/usr/sbin:/bin',
+    provider => shell
+}
+
+exec {'redirection':
+  command  => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me \/ permanent;/" /etc/nginx/sites-available/default',
   provider => shell,
-  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://google.com; listen 80; \\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart'
+}
+
+exec { 'allow firewall':
+    command => 'ufw allow "NGINX Full"',
+    path    => '/usr/bin:/usr/sbin:/bin',
+  provider  => shell,
+}
+
+exec { 'restart nginx':
+    command => 'sudo service nginx restart',
+    path    => '/usr/bin:/usr/sbin:/bin',
+  provider  => shell,
+
 }
